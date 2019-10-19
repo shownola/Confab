af928fe2ef70428be082af02413535288bbce473
shownola <swasieko@gmail.com>
2019-10-19 09:36:59 -0700

    Add message resource and create partial for displaying messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 
-validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 50 }
+validates :username, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: { case_sensitive: false }
+has_many :messages
 has_secure_password
 end

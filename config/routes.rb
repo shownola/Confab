6a89e64c14b0a89732a32684ce018490c9d7c7c3
shownola <swasieko@gmail.com>
2019-10-21 13:45:47 -0700

    Generate a chatroom channel

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
   post 'message', to: 'messages#create'
+
+
+  mount ActionCable.server, at: '/cable'
 end
